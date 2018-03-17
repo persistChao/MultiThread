a4df728e371b00e50a1a906fd9b3b212fa050075
persistsuchao@hotmail.com <persistsuchao@hotmail.com>
2018-03-17 19:07:26 +0800

    CAs 操作

---
+++ multithreadstudy/src/main/java/com/answer/thread/chapter4/atomic/AtomicReferenceDemo.java
@@ -0,0 +1,10 @@
+package com.answer.thread.chapter4.atomic;
+
+import java.util.concurrent.atomic.AtomicReference;
+
+/**
+ * Created by chao on 2018/3/17.
+ */
+public class AtomicReferenceDemo {
+    public static AtomicReference<Integer> money = new AtomicReference<Integer>();
+}
