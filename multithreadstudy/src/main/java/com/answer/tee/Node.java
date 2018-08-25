0a18d61787843f9a12385cec64adf954d2b84452
suchao <persistsuchao@hotmail.com>
2018-08-25 16:35:08 +0800

    实现栈，字符串反转，树

---
+++ multithreadstudy/src/main/java/com/answer/tee/Node.java
@@ -0,0 +1,15 @@
+package com.answer.tee;
+
+/**
+ * 二叉树节点
+ * Created by chao on 2018/8/25.
+ */
+public class Node {
+    private Object data;
+    private Node leftChild;
+    private Node rightChild;
+
+    public void display() {
+        System.out.println(data);
+    }
+}
