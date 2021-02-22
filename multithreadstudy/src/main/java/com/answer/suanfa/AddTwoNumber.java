96e3baed1d08173870d5ec6e31d4a840c46a973f
suchao <suchao@01zhuanche.com>
2021-02-22 16:29:12 +0800

    数据结构

--- multithreadstudy/src/main/java/com/answer/suanfa/AddTwoNumber.java
+++ multithreadstudy/src/main/java/com/answer/suanfa/AddTwoNumber.java
@@ -5,13 +5,16 @@ import java.util.HashMap;
 import java.util.Map;
 
 /**
+ * 给定一个数组 一个目标值 使得 数组中两个数相加等于目标值，返回数组下标
+ * 如 int[] {3,4,7,14,13,11,5,0} 7 target 10
+ * 返回[2,0]
  * @author answer
  * @version 1.0
  * @date 2021/2/4 6:41 下午
  */
 public class AddTwoNumber {
 
-    //int[] {3,4,7,14,13,11,5,0} 7 target 10
+
     public static int[] twoSum(int[] array , int target ) {
         Map<Integer, Integer> map = new HashMap<>();
         int[] resturn  = new int[2];
@@ -30,9 +33,9 @@ public class AddTwoNumber {
     }
 
     public static void main(String[] args) {
-//        int[] numbers = {3,4,7,14,13,11,5,0};
-//        int target = 10;
-//        System.out.println(Arrays.toString(twoSum(numbers, target)));
-        System.out.println(Math.max(10, 4));
+        int[] numbers = {3,4,7,14,13,11,5,0};
+        int target = 10;
+        System.out.println(Arrays.toString(twoSum(numbers, target)));
+
     }
 }
